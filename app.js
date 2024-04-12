82c1ccec6e06b31140f652f22fb3e0dae147a7ae
kothasravani <112386396+kothasravani@users.noreply.github.com>
2024-04-11 23:55:56 -0500

    Passport installed and updated in pug and added account in app.js

--- app.js
+++ app.js
@@ -89,3 +89,42 @@ console.error(err)
 }
 let reseed = true;
 if (reseed) {recreateDB();}
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+ )
+
+ app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+ // passport config
+// Use the existing connection
+// The Account model 
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
