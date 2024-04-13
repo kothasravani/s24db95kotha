5c305ee85b55cb0a164e9ecd9d368f0d88d82090
kothasravani <112386396+kothasravani@users.noreply.github.com>
2024-04-13 13:25:43 -0500

    login and register

--- app.js
+++ app.js
@@ -4,92 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var oresRouter = require('./routes/ores');
-var gridsRouter = require('./routes/grid');
-var pickRouter = require('./routes/pick');
-var ores = require("./models/ores");
-var resourceRouter = require('./routes/resource');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/ores', oresRouter);
-app.use('/grid', gridsRouter);
-app.use('/pick', pickRouter);
-app.use('/resource', resourceRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
-require('dotenv').config();
-const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-
-// We can seed the collection if needed on
-//server start
-async function recreateDB(){
-// Delete everything
-await ores.deleteMany();
-let instance1 = new
-ores({ore_name: 'Iron Ore', quality_level: 'High Grade', quantity_available: 1000});
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
-let instance2 = new
-ores({ore_name: 'Gold Ore', quality_level: 'Pure', quantity_available: 50});
-instance2.save().then(doc=>{
-console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
-});
-let instance3 = new
-ores({ore_name: 'Silver Ore', quality_level: 'Standard', quantity_available: 2000});
-instance3.save().then(doc=>{
-console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
-});
-}
-let reseed = true;
-if (reseed) {recreateDB();}
-
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
@@ -112,7 +26,38 @@ passport.use(new LocalStrategy(
   })
  )
 
- app.use(require('express-session')({
+var mongoose=require('mongoose');
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var mineralRouter = require('./routes/ores');
+var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
+var ores = require('./models/ores');
+var resourceRouter = require('./routes/resource');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
@@ -120,7 +65,16 @@ passport.use(new LocalStrategy(
  app.use(passport.initialize());
  app.use(passport.session());
 
- // passport config
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/ores', mineralRouter);
+app.use('/grid',gridRouter);
+app.use('/pick',pickRouter);
+app.use('/resource',resourceRouter);
+
+// passport config
 // Use the existing connection
 // The Account model 
 var Account =require('./models/account');
@@ -128,3 +82,48 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+// We can seed the collection if needed on server start
+async function recreateDB(){
+ // Delete everything
+ await ores.deleteMany();
+ let instance1 = new 
+ores({ore_name:"Brass", quality_level: "Standard",quantity_available: 1849});
+ instance1.save().then(doc=>{
+ console.log("First object saved")}
+ ).catch(err=>{
+ console.error(err)
+ });
+ let instance2 = new
+ ores({ore_name:"Gold", quality_level: "Good",quantity_available: 1999});
+ instance2.save().then(doc=>{
+ console.log("second object saved")}
+ ).catch(err=>{
+ console.error(err)
+ });
+ let instance3 = new
+ ores({ore_name:"Bauxite", quality_level: "pure",quantity_available: 1834});
+ instance3.save().then(doc=>{
+ console.log("third object saved")}
+ ).catch(err=>{
+ console.error(err)
+ });
+}
+let reseed = true;
+if (reseed) {recreateDB();}
